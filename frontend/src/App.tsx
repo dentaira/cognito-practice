69f4e8a099efb2788c299d591dd2a42e15c3ef7a
dentaira <heismanreport.05@gmail.com>
2024-05-04 13:52:39 +0700

    ログイン画面 追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useAuth } from "./hooks/use-auth";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import { SignIn } from "./pages/SignIn";
 
 function App() {
-  const [count, setCount] = useState(1000)
+  const auth = useAuth();
+
+  if (auth.isLoading) {
+    return <div></div>;
+  }
+
+  const TopPage = () => (
+    <div>
+      <p>トップページ</p>
+      <p>{auth.isAuthenticated ? "ログイン済" : "未ログイン"}</p>
+      <p>
+        <Link to="/signin">ログイン</Link>
+      </p>
+    </div>
+  );
+
+  const PrivateDashboard = () => (
+    <PrivateRoute>
+      <div>ようこそ！ {auth.username} さん！</div>
+      <button onClick={() => auth.signOut()}>ログアウト</button>
+    </PrivateRoute>
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React v2</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<TopPage />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="dashboard" element={<PrivateDashboard />}></Route>
+        <Route path="*" element={<p>Page Not Found</p>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
